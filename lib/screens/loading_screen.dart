afee4bb3fa76933927688837f6374dbf595c217b
shmll <shmllslmt@gmail.com>
2023-04-04 15:55:26 +0800

    Fixed!

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/services/location.dart';
 import 'package:http/http.dart';
-//TODO: Step 16 - Import dart:convert
+import 'dart:convert';
 
 const String apiKey = '93228ccfc3f4697fe4ff3c5014d01d20';
 
@@ -13,9 +13,12 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  //TODO: Step 18 - Create two double variables: latitude and longitude
+  //Create two double variables that will hold the latitude and longitude values
+  double? latitude;
+  double? longitude;
+
   //Create a getLocation() method that will get the current location of the device
-  void getLocation() async {
+  Future<void> getLocation() async {
     //Wrap in a try and catch block in the case that the device location cannot be retrieved
     try {
       //Create a Location object
@@ -23,15 +26,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
       //Call the getCurrentLocation() method
       await location.getCurrentLocation();
 
+      latitude = location.latitude;
+      longitude = location.longitude;
+
       //Print the latitude and longitude value
-      print(location.latitude);
-      print(location.longitude);
+      print(latitude);
+      print(longitude);
+
+      getData();
     } catch(e) {
       print(e);
     }
   }
-
-
 
   //Instead of having to click a button to print the current location of the device, print the current location straightaway when LoadingScreen appears
   @override
@@ -40,28 +46,41 @@ class _LoadingScreenState extends State<LoadingScreen> {
     getLocation();
   }
 
-
   //TODO: Step 23 - Combine the getLocation() and getData() method, create a NetworkHelper object in the method, call the getData() method
   //TODO: Step 24 - Go to LocationScreen using Navigator.push()
   //TODO: Step 28 - Pass the weatherData property to LocationScreen
 
   //Create a getData() method that will make an API call to openweathermap using the http get method
-  void getData() async {
-      var response = await get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=$apiKey'));
+  Future getData() async {
+    try {
+      //Use our very own latitude and longitude value
+      Response response = await get(Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric'));
 
       //Print the response body if the statusCode is equal to 200, else, print the statusCode
-      if(response.statusCode == 200) {
+      if (response.statusCode == 200) {
+        //Use jsonDecode to unpack the response body data (temp, weather condition, city name)
         print(response.body);
+
+        var data = response.body;
+        var weatherData = jsonDecode(data);
+        double temp = weatherData['main']['temp'];
+        int condition = weatherData['weather'][0]['id'];
+        String city_name = weatherData['name'];
+
+        print('Temp: $temp');
+        print('Condition: $condition');
+        print('City Name: $city_name');
       } else
         print(response.statusCode);
+    } catch(e) {
+      print(e);
+    }
   }
-  //TODO: Step 19 - Instead of using sample latitude and longitude, use our very own latitude and longitude value
-
-  //TODO: Step 17 - Use jsonDecode to unpack the response body data (temp, weather id, city name)
 
   @override
   Widget build(BuildContext context) {
-    getData();
+
     return Scaffold(
       body: Center(
         //TODO: Step 25 - Add the flutter spinkit dependency, then add the SpinKitDoubleBounce widget as the child of the Center widget
